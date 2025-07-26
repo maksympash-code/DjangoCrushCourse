23d53c036d9ec60912976ee3fb4df173ae666ac5
maksympash-code <maksympash@knu.ua>
2025-07-26 20:38:14 +0300

    Forms

--- generator/views.py
+++ generator/views.py
@@ -6,5 +6,5 @@ from django.http import HttpResponse
 def home(request):
     return render(request, 'generator/home.html', {'password' : 'gesijgiesjso11324'})
 
-def eggs(request):
-    return HttpResponse('<h1>Eggs are so tasty.</h1>')
+def password(request):
+    return render(request, 'generator/password.html')
