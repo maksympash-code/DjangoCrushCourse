c673c222182e4e8cce0bd900fb262b0ecf278570
maksympash-code <maksympash@knu.ua>
2025-07-27 16:35:15 +0300

    Homework

--- generator/views.py
+++ generator/views.py
@@ -25,3 +25,7 @@ def password(request):
         the_password += random.choice(characters)
         
     return render(request, 'generator/password.html', {'password': the_password})
+
+
+def author(request):
+    return render(request, 'generator/author.html')
